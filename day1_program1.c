c413bff7f0e1c1eb62d1f8fb4e8e73413a869d2f
parmida-sahni31 <parmida.sahni@gmail.com>
2025-09-07 16:11:56 +0530

    add program

---
+++ day1_program1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main ()
+{
+    int a,b,sum;
+    printf("input the first number: ");
+    scanf("%d", &a);
+    printf("enter the second number: ");
+    scanf("%d", &b);
+    sum= a + b;
+    printf("the sum is: %d", sum);
+    return 0;
+}
